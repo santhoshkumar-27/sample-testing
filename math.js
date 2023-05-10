d5b1f2ebb76af8d409a1b7b9a2b932018a04f2d2
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-10 07:06:46 +0530

    moving logic into seperate file

---
+++ math.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
--- sample_testing_file.js
+++ sample_testing_file.js
@@ -1,11 +1,3 @@
-function sum(a, b) {
-    return a + b;
-}
-function substract(a, b) {
-    return a - b;
-}
-
-
 let result = sum(5, 5);
 let expected = 10;
 if (result != expected) {
