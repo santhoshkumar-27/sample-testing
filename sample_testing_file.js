078b2f7c47d5349ca68cebd9a160eeffaf377450
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-10 07:02:13 +0530

    initial commit

---
+++ sample_testing_file.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a - b;
+}
+function substract(a, b) {
+    return a - b;
+}
+
+
+let result = sum(5, 5);
+let expected = 10;
+if (result != expected) {
+    throw new Error(`${result} is not equal to the ${expected}`)
+}
+
+
+result = sum(5, 5);
+expected = 10;
+if (result != expected) {
+    throw new Error(`${result} is not equal to the ${expected}`)
+}
