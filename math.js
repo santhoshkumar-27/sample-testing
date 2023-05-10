5a64c44d883479b6700d2bc113401498c99c8587
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-10 07:34:31 +0530

    feat: added new assertion method

--- math.js
+++ math.js
@@ -1,6 +1,13 @@
-function sum(a, b) {
-    return a + b;
-}
-function substract(a, b) {
-    return a - b;
+// function sum(a, b) {
+//     return a + b;
+// }
+// function substract(a, b) {
+//     return a - b;
+// }
+
+const sum = (a, b) => a + b;
+const substract = (a, b) => a - b;
+module.exports = {
+    sum,
+    substract
 }
--- sample_testing_file.js
+++ sample_testing_file.js
@@ -1,3 +1,6 @@
+// import {sum, substract} from './math';
+const { sum, substract } = require('./math')
+
 let result = sum(5, 5);
 let expected = 10;
 if (result != expected) {
@@ -5,8 +8,21 @@ if (result != expected) {
 }
 
 
-result = substract(5, 5);
-expected = 0;
-if (result != expected) {
-    throw new Error(`${result} is not equal to the ${expected}`)
+result = substract(15, 5);
+expect(result).toBe(0);
+expect(result).toBeTruthy();
+
+function expect(actual) {
+    return {
+        toBe(expected) {
+            if (actual != expected) {
+                throw new Error(`${actual} is not equal to the ${expected}`)
+            }
+        },
+        toBeTruthy() {
+            if (!!!actual) {
+                throw new Error(`${actual} is not to be truthy`)
+            }
+        }
+    }
 }
