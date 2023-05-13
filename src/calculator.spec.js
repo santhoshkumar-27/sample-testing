32f566cb64274a3c9c09c93c4600d7d593b757e2
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-13 09:42:46 +0530

    fix: seleinum issues

--- src/calculator.spec.js
+++ src/calculator.spec.js
@@ -1,4 +1,21 @@
-import { Calculator } from './calculator'
+ class Calculator {
+    constructor() {
+    }
+    add(a, b) {
+        return a + b;
+    }
+    subtract(a, b) {
+        return a - b;
+    }
+    multiply(a, b) {
+        return a * b;
+    }
+    divide(a, b) {
+        return a / b;
+    }
+}
+
+
 describe('calculator.js testing', () => {
     let calculator;
     beforeEach(() => {
