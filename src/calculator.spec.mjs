3e156a7f9abcc159e8995a719a2a2a4f0dff5aaa
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-13 08:56:48 +0530

    feat: added package of jasmine

---
+++ src/calculator.spec.mjs
@@ -0,0 +1,23 @@
+import { Calculator } from './calculator'
+describe('calculator.js testing', () => {
+    let calculator;
+    beforeEach(() => {
+        calculator = new Calculator()
+    })
+    it('should add two numbers passing from parameters', () => {
+        const result = calculator.add(1, 2);
+        expect(result).toBe(3)
+    })
+    it('should subtract two numbers passing from parameters', () => {
+        const result = calculator.subtract(1, 2);
+        expect(result).toBe(-1)
+    })
+    it('should multiply two numbers passing from parameters', () => {
+        const result = calculator.multiply(1, 2);
+        expect(result).toBe(2)
+    })
+    it('should  divide two numbers passing from parameters', () => {
+        const result = calculator.divide(2, 1);
+        expect(result).toBe(2)
+    })
+});
